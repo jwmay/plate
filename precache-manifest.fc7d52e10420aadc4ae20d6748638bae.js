3f84d21fffce5d0012d2164d5e315cdd41b7ba6f
Joseph May <jwmay87@gmail.com>
2021-07-21 12:20:55 -0700

    Updates

---
+++ precache-manifest.fc7d52e10420aadc4ae20d6748638bae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49f806e7d6a535353534fa627b1c400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed01f3339c3bec74229",
+    "url": "/static/js/2.181ca1d8.chunk.js"
+  },
+  {
+    "revision": "fa417be6adce29023dca0ca31e8766b5",
+    "url": "/static/js/2.181ca1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0c6cdd3327f02b9939",
+    "url": "/static/js/main.ca48ed14.chunk.js"
+  },
+  {
+    "revision": "1913cdd2514373c90a34",
+    "url": "/static/js/runtime-main.b5523bd6.js"
+  },
+  {
+    "revision": "f927335d3f642de54d361e01ad3c4527",
+    "url": "/static/media/doc-may-profile.f927335d.jpg"
+  },
+  {
+    "revision": "c6d96dd45dec09bbaaa429d4d7f941e9",
+    "url": "/static/media/plate-background.c6d96dd4.png"
+  },
+  {
+    "revision": "1dcb2d722b411bc7f635ee958d8c27a6",
+    "url": "/static/media/plate-logo.1dcb2d72.png"
+  }
+]);
