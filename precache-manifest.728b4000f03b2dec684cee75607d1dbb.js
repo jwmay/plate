0b3dd116f277b0b53205fafcec52629e4325fedb
Joseph May <jwmay87@gmail.com>
2021-07-21 13:47:22 -0700

    Updates

---
+++ precache-manifest.728b4000f03b2dec684cee75607d1dbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fd2c866d60e0424f04f642c743b561",
+    "url": "/plate/index.html"
+  },
+  {
+    "revision": "aed01f3339c3bec74229",
+    "url": "/plate/static/js/2.181ca1d8.chunk.js"
+  },
+  {
+    "revision": "fa417be6adce29023dca0ca31e8766b5",
+    "url": "/plate/static/js/2.181ca1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f358ce13602c3127f3",
+    "url": "/plate/static/js/main.9c8f8942.chunk.js"
+  },
+  {
+    "revision": "9fc0a2875e4a060e23c8",
+    "url": "/plate/static/js/runtime-main.e61e3754.js"
+  },
+  {
+    "revision": "f927335d3f642de54d361e01ad3c4527",
+    "url": "/plate/static/media/doc-may-profile.f927335d.jpg"
+  },
+  {
+    "revision": "c6d96dd45dec09bbaaa429d4d7f941e9",
+    "url": "/plate/static/media/plate-background.c6d96dd4.png"
+  },
+  {
+    "revision": "1dcb2d722b411bc7f635ee958d8c27a6",
+    "url": "/plate/static/media/plate-logo.1dcb2d72.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e0f112efc196e19ee98f87e7419d400.js"
+  "/plate/precache-manifest.728b4000f03b2dec684cee75607d1dbb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
